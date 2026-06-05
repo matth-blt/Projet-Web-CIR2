0eb1437a59a8ebffe0d8d505aa6147cde7cc05e4
AllAn0Ah <noah.guichard21@gmail.com>
2026-06-05 13:29:34 +0200

    update constants

--- api/constants.php
+++ api/constants.php
@@ -1,8 +1,8 @@
 <?php 
 
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'irve_bdd_projet');
+define('DB_USER', 'irveuser');
+define('DB_PASSWORD', 'irvepwd');
+define('DB_NAME', 'irve');
 define('DB_SERVER', 'localhost');
 define('DB_PORT', '3306');
 
