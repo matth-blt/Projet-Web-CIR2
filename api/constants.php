22f07167b800d6fe2c49fa523651e2aba445944f
Matthieu Ballout <136253755+matth-blt@users.noreply.github.com>
2026-06-09 08:37:49 +0200

    Update constants.php

--- api/constants.php
+++ api/constants.php
@@ -1,7 +1,7 @@
 <?php 
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'irve_bdd_projet');
+    define('DB_USER', 'ivreuser');
+    define('DB_PASSWORD', 'irvepwd');
+    define('DB_NAME', 'irve');
     define('DB_SERVER', 'localhost');
     define('DB_PORT', '3306');
 ?>
